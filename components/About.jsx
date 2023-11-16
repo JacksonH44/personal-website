7d9643f58b8aec49809be5ad3f204334c3665115
Jackson Howe <jacksonhowe3@gmail.com>
2023-11-15 21:08:53 -0500

    update about section to include photo of me

--- components/About.jsx
+++ components/About.jsx
@@ -11,12 +11,19 @@ const About = () => {
             I am a dedicated and curious student at the University of Waterloo who enjoys solving problems using both quantitative and qualitative approaches. In 2021, I took my first programming class in university and loved it so much, I switched my major from business to computer science. I've been using technology ever since then to explore and solve problems that I find fascinating.
           </p>
           <p className='py-2 text-gray-600'>
-            I've been using data-driven and machine learning approaches to solve problems related to statistical physics and digital oncology. I've also been learning web development - I made this website with React.js, Next.js, and Tailwind CSS. In my spare time I enjoy listening to music (my current favourites are The Cranberries, The Smiths, and Crystal Castles), watching baseball with my girlfriend, and long-distance running. I love meeting new people, traveling, and storytelling.
+            I've been using data-driven and machine learning approaches to solve problems related to statistical physics and digital oncology. I've also been learning web development - I made this website with React.js, Next.js, and TailwindCSS. In my spare time I enjoy listening to music (my current favourites are The Cranberries, The Smiths, and Crystal Castles), watching baseball with my girlfriend, and long-distance running. I love meeting new people, traveling, and storytelling.
           </p>
           <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p>
         </div>
-        <div className='w-full h-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <img className='rounded-xl' src="../assets/aboutImagePsycho.jpg" alt="/" />
+        <div className='w-full h-auto flex flex-col items-center justify-center'>
+          <div className='w-full h-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+            <img className='rounded-xl' src="../assets/brewing.JPG" alt="/" />
+          </div>
+          <div className='text-center text-gray-800 py-8'>
+            <p>
+              Me (right), with my friend James (left) at the old Heineken brewing factory in Amsterdam (He is on a stool, that's why I look so short!)
+            </p>
+          </div>
         </div>
       </div>
     </div>
