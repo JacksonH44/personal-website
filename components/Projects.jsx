90621fd119fb1a6ea4161f9ca71c15d366ba218a
Jackson Howe <jacksonhowe3@gmail.com>
2024-08-17 14:38:09 -0400

    feat(projects): add UHN work to project items (#5)

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import slideSleuthImg from '../public/assets/projects/mars_building.jpg'
 import CCIImg from '../public/assets/projects/design_thinking.jpg'
 import kuramotoImg from '../public/assets/projects/brain_network.jpg'
-import primalityImg from '../public/assets/projects/enigma_machine.jpg'
+import uhnImg from '../public/assets/projects/pmccrt.jpg'
 import ProjectItem from './ProjectItem'
 
 const Projects = () => {
@@ -13,14 +13,14 @@ const Projects = () => {
         <h2 className='py-4'>What I've Worked On</h2>
         <div className='grid md:grid-cols-2 gap-8'>
 
+        <ProjectItem title='University Health Network' backgroundImg={uhnImg} projectUrl='/UHNDataPipeline' tech='Python | pytest | Github Actions | Docker | Pandas'/>
+
         <ProjectItem title='SlideSleuth: Deep Learning Oncology' backgroundImg={slideSleuthImg} projectUrl='/slideSleuth' tech='Python | Tensorflow | Pandas | numPy | R' />
 
         <ProjectItem title='Clean Coffee Initiative: Design Thinking' backgroundImg={CCIImg} projectUrl='/cci' tech='Figma | UI/UX Design | Product Design' />
 
         <ProjectItem title='KuramotoESN: Neuroscience-Informed Machine Learning' backgroundImg={kuramotoImg} projectUrl='/kuramoto' tech='MATLAB | C | Bash'/>
-
-        <ProjectItem title='Cryptography: Primality Testing' backgroundImg={primalityImg} projectUrl='/primalityTesting' tech='Python'/>
-
+        
         </div>
       </div>
     </div>
