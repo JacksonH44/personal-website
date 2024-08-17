9f61c15fb47a865b1ee5d79c24d2612bfe9a473b
Jackson Howe <jacksonhowe3@gmail.com>
2024-08-17 14:01:54 -0400

    perf: update next/image to next/legacy/image (#5)

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/legacy/image"
 import React from 'react'
 
 import businessAcumenImg from '../public/assets/skills/business_acumen.png'
--- pages/primalityTesting.jsx
+++ pages/primalityTesting.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import Link from 'next/link'
 import React from 'react';
 import primalityImg from '../public/assets/projects/enigma_machine.jpg'
