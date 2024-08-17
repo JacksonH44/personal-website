086009b2d05a3006b2e5570ab0a9a423419d3ee4
Jackson Howe <jacksonhowe3@gmail.com>
2024-08-17 14:53:30 -0400

    feat(projects): add Memento to project items (#5)

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import slideSleuthImg from '../public/assets/projects/mars_building.jpg'
 import CCIImg from '../public/assets/projects/design_thinking.jpg'
-import kuramotoImg from '../public/assets/projects/brain_network.jpg'
+import mementoImg from '../public/assets/projects/mindfulness.jpg'
 import uhnImg from '../public/assets/projects/pmccrt.jpg'
 import ProjectItem from './ProjectItem'
 
-const Projects = () => {
+export default function Projects() {
   return (
     <div id='projects' className='w-full'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className='text-xl tracking-widest uppercase text-[#5438dc]'>Projects</p>
         <h2 className='py-4'>What I've Worked On</h2>
         <div className='grid md:grid-cols-2 gap-8'>
+        
+        <ProjectItem title='Memento: Full-stack Mindfullness App' backgroundImg={mementoImg} projectUrl='/Memento' tech='Kotlin | Jetpack Compose | Android Studio'/>
 
         <ProjectItem title='University Health Network' backgroundImg={uhnImg} projectUrl='/UHNDataPipeline' tech='Python | pytest | Github Actions | Docker | Pandas'/>
 
@@ -19,12 +21,8 @@ const Projects = () => {
 
         <ProjectItem title='Clean Coffee Initiative: Design Thinking' backgroundImg={CCIImg} projectUrl='/cci' tech='Figma | UI/UX Design | Product Design' />
 
-        <ProjectItem title='KuramotoESN: Neuroscience-Informed Machine Learning' backgroundImg={kuramotoImg} projectUrl='/kuramoto' tech='MATLAB | C | Bash'/>
-        
         </div>
       </div>
     </div>
   )
 }
-
-export default Projects
